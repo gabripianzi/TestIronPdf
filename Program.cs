ad5d4851b1e809b8daadb5657333a4103e51ba85
Gabriele Pianzi <gabriele.pianzi@advantis.it>
2024-07-11 10:44:10 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using IronPdf.Editing;
+using IronSoftware.Drawing;
+
+if(File.Exists("License.txt"))
+    License.LicenseKey = File.ReadAllText("License.txt");
+
+
+var pdf = new PdfDocument("input.pdf");
+
+
+for (var i = 0; i < pdf.PageCount; i++)
+{
+    // attach to page
+    var qrStamp = new BarcodeStamper(i.ToString(), BarcodeEncoding.QRCode)
+    {
+        Scale = 40,
+        HorizontalAlignment = HorizontalAlignment.Right,
+        VerticalAlignment = VerticalAlignment.Top,
+        VerticalOffset = new Length(50, MeasurementUnit.Pixel),
+        HorizontalOffset = new Length(30, MeasurementUnit.Pixel),
+        IsStampBehindContent = true
+    };
+
+    pdf.ApplyStamp(qrStamp, i);
+
+
+}
+
+
+pdf.SaveAs("output.pdf");
---
+++ README.md
@@ -0,0 +1,8 @@
+# Test project for bug in IronPdf
+
+In the root you should insert in the root two files: License.txt and input.pdf 
+License.txt should contain the license
+
+The script take the input.pdf file, apply a barcode on each page and export the file in the output.pdf file.
+
+As you can see comparing the input and output file, some of the character of the pdf che erased or replaces.
